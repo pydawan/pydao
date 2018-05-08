caa58aaa7f39b03ecb120fb5d7562d32f5e10a34
Thiago Alexandre <thicmp@gmail.com>
2018-05-08 16:47:44 -0300

    1 - Atualização do PYTHONPATH através da instrução: sys.path.append('.') 2 - Habilitado auto commit na conexão com o banco de dados.

--- pydao/orm/main.py
+++ pydao/orm/main.py
@@ -10,22 +10,27 @@ Created on 28/03/2014
 __author__ = 'Thiago Alexandre Martins Monteiro'
 __date__ = '23/11/2012'
 
-from db import DriverManager as dm
-import dao
-
 if __name__ == '__main__':
+
+    import sys
+    import dao
+
+    sys.path.append('.')
+
+    from db import DriverManager as dm
     from pydao.orm.models import Produto
+
     # Creating a model class instance.
     produto = Produto()
     produto.id = 1
     produto.nome = 'CD George Michael'
     produto.preco = 21.00    
     print '%s' % produto    
-    conn = dm.connection(dbms='mysql', user='pydao', password='pydao', database='pydao')
+    conn = dm.connection(dbms='mysql', user='pydao', password='pydao', database='pydao', auto_commit=True)
     print conn
     # Creating a instance of the GenericDAO class.
     dao = dao.GenericDAO(model=Produto, connection=conn)
     dao.insert(produto)
-#    for r in dao.select():
-#        print r
+    for r in dao.select():
+        print r
     conn.close()
